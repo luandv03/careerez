e192a737da4452f8058721ac0fe9ebc700ae78f2
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-11-19 00:36:50 +0700

    build scroll on top butto,

--- client/src/layouts/LayoutApp/LayoutApp.tsx
+++ client/src/layouts/LayoutApp/LayoutApp.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "@mantine/core";
+import { useWindowScroll } from "@mantine/hooks";
+import { IconArrowBigUp } from "@tabler/icons-react";
 
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import styles from "./LayoutApp.module.css";
 
 export default function LayoutApp() {
+    const [scroll, scrollTo] = useWindowScroll();
+
+    const handleGoToTop = () => {
+        scrollTo({ x: 0, y: 0 });
+    };
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
@@ -16,6 +25,19 @@ export default function LayoutApp() {
             <footer className={styles.footer}>
                 <Footer />
             </footer>
+
+            <Button
+                bg="rgb(40, 89, 182)"
+                style={{
+                    position: "fixed",
+                    right: "24px",
+                    bottom: "100px",
+                    display: scroll.y > 600 ? "flex" : "none",
+                }}
+                onClick={() => handleGoToTop()}
+            >
+                <IconArrowBigUp />
+            </Button>
         </div>
     );
 }
