11c93ce5ad7397516fd932638b289d2576548bd4
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-11-23 00:10:04 +0700

    build ui member register

--- client/src/components/MemberRegister/MemberRegister.tsx
+++ client/src/components/MemberRegister/MemberRegister.tsx
@@ -1,3 +1,271 @@
+import { Box, Center, Grid, Stack, Text, List, Button } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+
+import classes from "./MemberRegister.module.css";
+
 export const MemberRegister = () => {
-    return <div>Register Service</div>;
+    const navigate = useNavigate();
+
+    const handleRegsiterService = () => {
+        // check login
+        if (!JSON.parse(localStorage.getItem("isAuthenticated") as string)) {
+            navigate("/signin");
+        }
+    };
+
+    return (
+        <Stack p={50}>
+            <Center>
+                <Text fw={700} size="30px">
+                    ĐĂNG KÍ THÀNH VIÊN
+                </Text>
+            </Center>
+            <Center>
+                <Text fw={500} size="20px" align="center">
+                    Đăng kí thành viên là cách tốt nhất để bạn trải nghiệm toàn
+                    bộ nền tảng của chúng tôi và nhận được nhiều ưu đãi độc
+                    quyền. Hãy cùng CareerEZ hướng đến sự phát triển cá nhân và
+                    nghề nghiệp tốt hơn.
+                </Text>
+            </Center>
+
+            <Center>
+                <Grid style={{ width: "100%" }} gutter="xl" p={50}>
+                    <Grid.Col span={4} h={600}>
+                        <Box
+                            h={500}
+                            style={{
+                                width: "100%",
+                                borderRadius: "17px",
+                            }}
+                            p={20}
+                            className={classes.packageService}
+                        >
+                            <Stack
+                                justify="space-between"
+                                style={{ height: "100%", width: "100%" }}
+                            >
+                                <Stack style={{ width: "100%" }}>
+                                    <Center>
+                                        <Text fw={700} size="20px">
+                                            GÓI CƠ BẢN
+                                        </Text>
+                                    </Center>
+                                    <Center>
+                                        <Text
+                                            fw={700}
+                                            size="20px"
+                                            color="rgb(255,222,89)"
+                                        >
+                                            Miễn phí
+                                        </Text>
+                                    </Center>
+                                    <List>
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Chọn 1 nhóm ngành
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Chọn 1 công ty theo đúng nhóm
+                                                ngành đó
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được tham gia các nhiệm vụ cơ
+                                                bản
+                                            </Text>
+                                        </List.Item>
+                                    </List>
+                                </Stack>
+
+                                <Center>
+                                    <Button
+                                        w={200}
+                                        className={classes.btnRegister}
+                                        onClick={() => handleRegsiterService()}
+                                    >
+                                        <Text>Đăng ký</Text>
+                                    </Button>
+                                </Center>
+                            </Stack>
+                        </Box>
+                    </Grid.Col>
+
+                    <Grid.Col span={4} h={600}>
+                        <Box
+                            h={500}
+                            style={{
+                                width: "100%",
+                                borderRadius: "17px",
+                            }}
+                            p={20}
+                            className={classes.packageService}
+                        >
+                            <Stack
+                                justify="space-between"
+                                style={{ height: "100%", width: "100%" }}
+                            >
+                                <Stack style={{ width: "100%" }}>
+                                    <Center>
+                                        <Text fw={700} size="20px">
+                                            GÓI NÂNG CAO
+                                        </Text>
+                                    </Center>
+                                    <Center>
+                                        <Text
+                                            fw={700}
+                                            size="20px"
+                                            color="rgb(255,222,89)"
+                                        >
+                                            Miễn phí
+                                        </Text>
+                                    </Center>
+                                    <List>
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Chọn 1 nhóm ngành
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Chọn 2 công ty theo đúng nhóm
+                                                ngành đó
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được tham gia toàn bộ các nhiệm
+                                                vụ với sự hướng dẫn của mentor
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được giấy chứng nhận khi hoàn
+                                                thành các nhiệm vụ
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được tham gia miễn phí các
+                                                workshop của chuyên gia hàng
+                                                tháng
+                                            </Text>
+                                        </List.Item>
+                                    </List>
+                                </Stack>
+
+                                <Center>
+                                    <Button
+                                        w={200}
+                                        className={classes.btnRegister}
+                                        onClick={() => handleRegsiterService()}
+                                    >
+                                        <Text>Đăng ký</Text>
+                                    </Button>
+                                </Center>
+                            </Stack>
+                        </Box>
+                    </Grid.Col>
+
+                    <Grid.Col span={4} h={600}>
+                        <Box
+                            h={500}
+                            style={{
+                                width: "100%",
+                                borderRadius: "17px",
+                            }}
+                            className={classes.packageService}
+                            p={20}
+                        >
+                            <Stack
+                                justify="space-between"
+                                style={{ height: "100%", width: "100%" }}
+                            >
+                                <Stack style={{ width: "100%" }}>
+                                    <Center>
+                                        <Text fw={700} size="20px">
+                                            GÓI ĐẶC BIỆT
+                                        </Text>
+                                    </Center>
+                                    <Center>
+                                        <Text
+                                            fw={700}
+                                            size="20px"
+                                            color="rgb(255,222,89)"
+                                        >
+                                            Miễn phí
+                                        </Text>
+                                    </Center>
+                                    <List>
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Chọn 2 nhóm ngành
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Chọn 2 công ty theo đúng nhóm
+                                                ngành đó
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được tham gia toàn bộ các nhiệm
+                                                vụ với sự hướng dẫn 1:1 của
+                                                mentor
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được nhận thư giới thiệu từ
+                                                chuyên gia của công ty khi hoàn
+                                                thành
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được giấy chứng nhận khi hoàn
+                                                thành các nhiệm vụ
+                                            </Text>
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Text fw={500} size="18px">
+                                                Được tham gia miễn phí các
+                                                workshop của chuyên gia hàng
+                                                tháng
+                                            </Text>
+                                        </List.Item>
+                                    </List>
+                                </Stack>
+
+                                <Center>
+                                    <Button
+                                        w={200}
+                                        className={classes.btnRegister}
+                                        onClick={() => handleRegsiterService()}
+                                    >
+                                        <Text>Đăng ký</Text>
+                                    </Button>
+                                </Center>
+                            </Stack>
+                        </Box>
+                    </Grid.Col>
+                </Grid>
+            </Center>
+        </Stack>
+    );
 };
