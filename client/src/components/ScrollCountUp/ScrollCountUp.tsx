9e3dc887fbafd1b10c46a885fbbb782f872cd4fe
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-11-25 15:42:31 +0700

    update countup number

--- client/src/components/ScrollCountUp/ScrollCountUp.tsx
+++ client/src/components/ScrollCountUp/ScrollCountUp.tsx
@@ -1,6 +1,4 @@
 import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
-import { useState } from "react";
 
 export const ScrollCountUp = ({
     number,
@@ -13,31 +11,23 @@ export const ScrollCountUp = ({
     decimal?: string;
     decimals?: number;
 }) => {
-    const [counterOn, setCounterOn] = useState(false);
-
     return (
-        <ScrollTrigger
-            onEnter={() => setCounterOn(true)}
-            onExit={() => setCounterOn(false)}
+        <CountUp
+            start={0.001}
+            end={number}
+            duration={2.5}
+            suffix={suffix}
+            decimal={decimal}
+            decimals={decimals}
+            enableScrollSpy
         >
-            {counterOn && (
-                <CountUp
-                    start={0.001}
-                    end={number}
-                    duration={2.5}
-                    suffix={suffix}
-                    decimal={decimal}
-                    decimals={decimals}
-                >
-                    {({ countUpRef }) => (
-                        <div>
-                            <h1>
-                                <span ref={countUpRef} />
-                            </h1>
-                        </div>
-                    )}
-                </CountUp>
+            {({ countUpRef }) => (
+                <div>
+                    <h1>
+                        <span ref={countUpRef} />
+                    </h1>
+                </div>
             )}
-        </ScrollTrigger>
+        </CountUp>
     );
 };
